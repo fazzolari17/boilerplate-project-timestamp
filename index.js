c044935cf3e46585443b7ffb29a7c62a20985122
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-12-27 13:41:16 -0500

    initial commit

--- index.js
+++ index.js
@@ -26,6 +26,48 @@ app.get("/api/hello", function (req, res) {
 
 
 
+app.get('/api/:date?', (req, res) => {
+  let date = req.params.date;
+  const testDateType = req.path.replace(/\/api\/?/, '');
+  const containsXml = /%/.test(testDateType);
+  let unix = new Date(date);
+  let utc = unix.toUTCString();
+
+
+  if(date === 'this-is-not-a-date') {
+    return res.json({
+      "error": "invalid Date"
+    });
+  };
+  if (!date) {
+    date = Date.now();
+    
+    return res.json({
+      "unix": Date.now(),
+      "utc": new Date(date).toUTCString()
+    })
+  };
+
+  const test = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/.test(date);
+
+  if(test) {
+    return res.json({
+      unix: unix.getTime(),
+      utc
+      })
+  } else if (!test) {
+    date = containsXml ? Date.parse(date) : parseInt(date);
+    unix = new Date(date);
+    utc = unix.toUTCString()
+
+    return res.json({
+      unix: date,
+      utc
+    })
+  }
+});
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
